16b05ed1c1ad1f6c4c88b17917e58b491c6ca254
mascarlosjavier <carlosjavieruribe@hotmail.com>
2017-03-28 11:32:33 -0500

    Wireframe2 final version

--- QueuePerformanceMeasures/src/frame/Wireframe2.java
+++ QueuePerformanceMeasures/src/frame/Wireframe2.java
@@ -2,6 +2,8 @@ package frame;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.*;
 
@@ -11,96 +13,161 @@ public class Wireframe2 extends JPanel implements ActionListener{
 	static String buttonCalcString = "Calculate";
 	static String lambdaString = "Lambda";
 	
-	//Objetos dentro del panel Wireframe1
-	JTextArea textarea;
+	//Objetos dentro del panel Wireframe2
+	JPanel titlePanel, labelPanel, textFieldPanel, centralPanel, lambdaPanel, muPanel;
+	JTextArea textf;
 	JButton buttonCalc;
 	JLabel lambdaLabel, muLabel, serversLabel, capacityLabel, varianceLabel, cvarrivalsLabel, cvserviceLabel;
-	JTextField lambdaTf, muTf, capacityTf, varianceTf, cvarrivalsTf, cvserviceTf;
-	JSpinner serverSpinner;
-	
-	public Wireframe2(){
+	JTextField lambdaTf, muTf, varianceTf, cvarrivalsTf, cvserviceTf;
+	JSpinner serverSpinner, capacitySpinner;
+
+    public static JComponent getTwoColumnLayout(
+            JLabel[] labels,
+            JComponent[] fields,
+            boolean addMnemonics) {
+        if (labels.length != fields.length) {
+            String s = labels.length + " labels supplied for "
+                    + fields.length + " fields!";
+            throw new IllegalArgumentException(s);
+        }
+        JComponent panel = new JPanel();
+        GroupLayout layout = new GroupLayout(panel);
+        panel.setLayout(layout);
+        // Turn on automatically adding gaps between components
+        layout.setAutoCreateGaps(true);
+        // Create a sequential group for the horizontal axis.
+        GroupLayout.SequentialGroup hGroup = layout.createSequentialGroup();
+        GroupLayout.Group yLabelGroup = layout.createParallelGroup(GroupLayout.Alignment.TRAILING);
+        hGroup.addGroup(yLabelGroup);
+        GroupLayout.Group yFieldGroup = layout.createParallelGroup();
+        hGroup.addGroup(yFieldGroup);
+        layout.setHorizontalGroup(hGroup);
+        // Create a sequential group for the vertical axis.
+        GroupLayout.SequentialGroup vGroup = layout.createSequentialGroup();
+        layout.setVerticalGroup(vGroup);
+
+        int p = GroupLayout.PREFERRED_SIZE;
+        // add the components to the groups
+        for (JLabel label : labels) {
+            yLabelGroup.addComponent(label);
+        }
+        for (Component field : fields) {
+            yFieldGroup.addComponent(field, p, p, p);
+        }
+        for (int ii = 0; ii < labels.length; ii++) {
+            vGroup.addGroup(layout.createParallelGroup().
+                    addComponent(labels[ii]).
+                    addComponent(fields[ii], p, p, p));
+        }
+
+        if (addMnemonics) {
+            addMnemonics(labels, fields);
+        }
+
+        return panel;
+    }
+
+    private final static void addMnemonics(
+            JLabel[] labels,
+            JComponent[] fields) {
+        Map<Character, Object> m = new HashMap<Character, Object>();
+        for (int ii = 0; ii < labels.length; ii++) {
+            labels[ii].setLabelFor(fields[ii]);
+            String lwr = labels[ii].getText().toLowerCase();
+            for (int jj = 0; jj < lwr.length(); jj++) {
+                char ch = lwr.charAt(jj);
+                if (m.get(ch) == null && Character.isLetterOrDigit(ch)) {
+                    m.put(ch, ch);
+                    labels[ii].setDisplayedMnemonic(ch);
+                    break;
+                }
+            }
+        }
+    }
+    
+	public static JComponent getTwoColumnLayout(
+            String[] labelStrings,
+            JComponent[] fields) {
+        JLabel[] labels = new JLabel[labelStrings.length];
+        for (int ii = 0; ii < labels.length; ii++) {
+            labels[ii] = new JLabel(labelStrings[ii]);
+        }
+        return getTwoColumnLayout(labels, fields);
+    }
+    
+    public static JComponent getTwoColumnLayout(
+            JLabel[] labels,
+            JComponent[] fields) {
+        return getTwoColumnLayout(labels, fields, true);
+    }
+    
+	private Wireframe2(){
+		//Crea la ventana
 		JFrame jfrme = new JFrame("Queue Performance Measures");
 		
+		//Opciones por defecto de la ventana
 		jfrme.setLayout(new FlowLayout());
-		jfrme.setSize(460, 450);
+		jfrme.setSize(380, 390);
 		jfrme.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jfrme.getContentPane().setBackground(Color.WHITE);
 		
-		textarea= new JTextArea("Insert Parameters");
-		textarea.setEditable(false);
+		//Panel con el título
+		titlePanel = new JPanel();
+		titlePanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		titlePanel.setPreferredSize(new Dimension(340, 80));
+		titlePanel.setLayout(new GridBagLayout());
+		titlePanel.setBackground(Color.WHITE);
+		textf= new JTextArea("Insert Parameters");
+		textf.setEditable(false);
+		textf.setFont(new Font("Arial", Font.PLAIN, 24));
+		titlePanel.add(textf);
 		
-		lambdaLabel = new JLabel(lambdaString);
-		lambdaTf = new JTextField();
-			
-		muLabel = new JLabel("Mu");
-		muTf = new JTextField();
-				
-		serversLabel = new JLabel("Number of servers");
-		
-		serverSpinner = new JSpinner();
-		serverSpinner.setValue(1);
-		
-		capacityLabel = new JLabel("Capacity");
-		capacityTf = new JTextField();
-		
-		varianceLabel = new JLabel("Variance");
-		varianceTf = new JTextField();
-		
-		cvarrivalsLabel = new JLabel("Coefficient of variation for interarrival times");
-		cvarrivalsTf = new JTextField();
-		
-		cvserviceLabel = new JLabel("Coefficient of variation for service times");
-		cvserviceTf = new JTextField(); 
+        JComponent[] components = {
+                new JTextField(5),
+                new JTextField(5),
+                new JSpinner(new SpinnerNumberModel(1,1,20,1)),
+                new JSpinner(new SpinnerNumberModel(1,1,20,1)),
+                new JTextField(5),
+                new JTextField(5),
+                new JTextField(5)
+            };
+
+            String[] labels = {
+                "Lambda:",
+                "Mu:",
+                "Number of servers:",
+                "Capacity:",
+                "Variance:",
+                "CV for interarrival times:",
+                "CV for service times:"
+            };
+            
+		JComponent labelsAndFields = getTwoColumnLayout(labels,components);
+		labelsAndFields.setBackground(Color.WHITE);	
+		labelsAndFields.setPreferredSize(new Dimension(240, 190));
+		labelPanel=new JPanel();
+		labelPanel.setPreferredSize(new Dimension(15, 180));
+		labelPanel.setBackground(Color.WHITE);
+		centralPanel = new JPanel();
 		
 		buttonCalc = new JButton(buttonCalcString);
 		
-		jfrme.add(textarea);
-		jfrme.add(lambdaLabel);
-		jfrme.add(lambdaTf);
-		jfrme.add(muLabel);
-		jfrme.add(muTf);
-		jfrme.add(serversLabel);
-		jfrme.add(serverSpinner);
-		jfrme.add(capacityLabel);
-		jfrme.add(capacityTf);
-		jfrme.add(varianceLabel);
-		jfrme.add(varianceTf);
-		jfrme.add(cvarrivalsLabel);
-		jfrme.add(cvarrivalsTf);
-		jfrme.add(cvserviceLabel);
-		jfrme.add(cvserviceTf);
-		jfrme.add(buttonCalc);
+		jfrme.add(titlePanel);
+		centralPanel.add(labelPanel);
+		centralPanel.add(labelsAndFields, BorderLayout.CENTER);
+		centralPanel.setBackground(Color.WHITE);
 		
+		jfrme.add(centralPanel);
+		jfrme.add(buttonCalc);
 		jfrme.setVisible(true);
 	}
 	
-	//Listen to Button
 	public void actionPerformed(ActionEvent ae){
 		
-		
 	}
-	
-    private static void createAndShowWireframe2() {
-        //Create and set up the window.
-        JFrame frame2 = new JFrame("Wireframe2");
-        frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Create and set up the content pane.
-        JComponent newContentPane = new Wireframe2();
-        newContentPane.setOpaque(true); //content panes must be opaque
-        frame2.setContentPane(newContentPane);
-
-        //Display the window.
-        frame2.pack();
-        //frame2.setVisible(true);
-    }
-    
+	 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-		SwingUtilities.invokeLater(new Runnable(){
-			public void run(){
-				createAndShowWireframe2();
-			}
-		});
+		new Wireframe2();
 	}
 }
